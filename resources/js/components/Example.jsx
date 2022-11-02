c2e3d9d3b8d04e619919f4ce82c58686948a8d6e
寺下大典 <ekusiad.tera@icloud.com>
2022-11-02 21:54:34 +0900

    削除機能の実装

--- resources/js/components/Example.jsx
+++ resources/js/components/Example.jsx
@@ -1,4 +1,4 @@
-import React,{Fragment,useState,useEffect} from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import Dialog from '@mui/material/Dialog';
@@ -12,134 +12,134 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 
-function Example(){
-    const [year,setYear] = useState(new Date().getFullYear())
-    const [month,setMonth] = useState(new Date().getMonth()+1)
-    const last = new Date(year,month,0).getDate()
-    const prevlast = new Date(year,month-1,0).getDate()
-    const calendar = createCalendear(year,month)
+function Example() {
+    const [year, setYear] = useState(new Date().getFullYear())
+    const [month, setMonth] = useState(new Date().getMonth() + 1)
+    const last = new Date(year, month, 0).getDate()
+    const prevlast = new Date(year, month - 1, 0).getDate()
+    const calendar = createCalendear(year, month)
 
     const onClick = n => () => {
         const nextMonth = month + n
         if (12 < nextMonth) {
-          setMonth(1)
-          setYear(year + 1)
+            setMonth(1)
+            setYear(year + 1)
         } else if (nextMonth < 1) {
-          setMonth(12)
-          setYear(year - 1)
+            setMonth(12)
+            setYear(year - 1)
         } else {
-          setMonth(nextMonth)
+            setMonth(nextMonth)
         }
     }
 
     //スケジュールのデータ
-    const [schedules,setSche] = useState([])
+    const [schedules, setSche] = useState([])
 
     //画面読み込み時に、1度だけ起動
-    useEffect(()=>{
+    useEffect(() => {
         getPostData();
-    },[])
+    }, [])
 
     //バックエンドからデータ一覧を取得
-    const getPostData = () =>{
+    const getPostData = () => {
         axios
-        .post('/api/posts')
-        .then(response=>{
-            setSche(response.data); //バックエンドからのデータをセット
-            console.log(response.data);
-        }).catch(()=>{
-            console.log('通信に失敗しました');
-        });
+            .post('/api/posts')
+            .then(response => {
+                setSche(response.data); //バックエンドからのデータをセット
+                console.log(response.data);
+            }).catch(() => {
+                console.log('通信に失敗しました');
+            });
     }
 
     //データ格納の空配列を作成
     let rows = [];
 
     //スケジュールデータをrowに格納する
-    schedules.map((post)=>
+    schedules.map((post) =>
         rows.push({
-            sch_id:post.id,
-            sch_category:post.sch_category,
-            sch_contents:post.sch_contents,
-            sch_date:post.sch_date,
-            sch_time:post.sch_time
+            sch_id: post.id,
+            sch_category: post.sch_category,
+            sch_contents: post.sch_contents,
+            sch_date: post.sch_date,
+            sch_time: post.sch_time
         })
     );
 
     //登録用ポップアップ開閉処理
-    const[open,setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
 
-    const handleClickOpen = (e) =>{
+    const handleClickOpen = (e) => {
         setOpen(true);
     };
 
-    const handleClose = () =>{
+    const handleClose = () => {
         setOpen(false);
     };
 
     //新規登録用データ配列
-    const [formData,setFormData] = useState({sch_category:'',sch_contents:'',sch_date:'',sch_hour:'',sch_min:''});
+    const [formData, setFormData] = useState({ sch_category: '', sch_contents: '', sch_date: '', sch_hour: '', sch_min: '' });
 
     //入力値を一時保存
-    const inputChange = (e) =>{
+    const inputChange = (e) => {
         const key = e.target.name;
         const value = e.target.value;
         formData[key] = value;
-        let datas = Object.assign({},formData);
+        let datas = Object.assign({}, formData);
         setFormData(datas);
     }
     //登録処理
-    const createSchedule = async() => {
+    const createSchedule = async () => {
         //入力値を投げる
         await axios
-            .post('/api/posts/create',{
-                sch_category:formData.sch_category,
-                sch_contents:formData.sch_contents,
-                sch_date:formData.sch_date,
-                sch_time:formData.sch_hour + ':' + formData.sch_min
+            .post('/api/posts/create', {
+                sch_category: formData.sch_category,
+                sch_contents: formData.sch_contents,
+                sch_date: formData.sch_date,
+                sch_time: formData.sch_hour + ':' + formData.sch_min
             })
-            .then((res)=>{
+            .then((res) => {
                 //戻り値をtodosにセット
                 const tempPosts = post;
                 tempPosts.push(res.data);
                 setPosts(tempPosts)
                 setFormData('');
             })
-            .catch(error=>{
+            .catch(error => {
                 console.log(error);
             })
     }
 
     //更新用ダイアログ開閉機能
-    const[editopen,setEditOpen] = useState(false);
+    const [editopen, setEditOpen] = useState(false);
 
-    const editHandleClickOpen = (e) =>{
+    const editHandleClickOpen = (e) => {
         e.stopPropagation();
         setEditOpen(true);
         getEditData(e);
     };
 
-    const editHandleClose = () =>{
+    const editHandleClose = () => {
         setEditOpen(false);
     };
 
     //更新用のデータ配列
-    const [editData,setEditData] = useState({id:'',sch_category:'',sch_contents:'',sch_date:'',sch_hour:'',sch_min:''});
+    const [editData, setEditData] = useState({ id: '', sch_category: '', sch_contents: '', sch_date: '', sch_hour: '', sch_min: '' });
 
     //バックエンドからデータ一覧を取得
-    function getEditData(e){
+    function getEditData(e) {
         axios
             .post('/api/edit', {
                 id: e.currentTarget.id
             })
             .then(res => {
                 setEditData({
-                    id:res.data.id,
-                    sch_category:res.data.sch_category,
-                    sch_contents:res.data.sch_contents,
-                    sch_date:res.data.sch_date,
-                    sch_hour:res.data.sch_time.substr(0,2),
-                    sch_min:res.data.sch_time.substr(3,2)
+                    id: res.data.id,
+                    sch_category: res.data.sch_category,
+                    sch_contents: res.data.sch_contents,
+                    sch_date: res.data.sch_date,
+                    sch_hour: res.data.sch_time.substr(0, 2),
+                    sch_min: res.data.sch_time.substr(3, 2)
                 });
             })
             .catch(() => {
@@ -148,36 +148,54 @@ function Example(){
     }
 
     //入力値を一時保存
-    const editChange = (e) =>{
+    const editChange = (e) => {
         const key = e.target.name;
         const value = e.target.value;
         editData[key] = value;
-        let datas = Object.assign({},editData);
+        let datas = Object.assign({}, editData);
         setEditData(datas);
     }
 
     //ダイアログデータを登録
-    const updateSchedule = async() => {
+    const updateSchedule = async () => {
         //入力値を投げる
         await axios
-            .post('/api/update',{
-                id:editData.id,
-                sch_category:editData.sch_category,
-                sch_contents:editData.sch_contents,
-                sch_date:editData.sch_date,
-                sch_time:editData.sch_hour + ':' + editData.sch_min
+            .post('/api/update', {
+                id: editData.id,
+                sch_category: editData.sch_category,
+                sch_contents: editData.sch_contents,
+                sch_date: editData.sch_date,
+                sch_time: editData.sch_hour + ':' + editData.sch_min
             })
-            .then((res)=>{
+            .then((res) => {
                 //戻り値をtodosにセット
                 setEditData(res.data);
             })
-            .catch(error=>{
+            .catch(error => {
                 console.log(error);
             })
     }
 
-    
+
     console.log(editData);
+
+    const deletePost = async (e) => {
+        //リンク移動の無効化
+        e.preventDefault()
+        //削除処理
+        await axios
+            .post('api/delete', {
+                id: editData.id
+            })
+            .then((res) => {
+                this.setState({
+                    posts: res.posts
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
 
     return (
         <Fragment>
@@ -195,19 +213,19 @@ function Example(){
                     </tr>
                 </thead>
                 <tbody>
-                    {calendar.map((week,i) => (
+                    {calendar.map((week, i) => (
                         <tr key={week.join('')}>
-                            {week.map((day,j) => (
+                            {week.map((day, j) => (
                                 <td key={`${i}${j}`} id={day} onClick={handleClickOpen}>
                                     <div>
                                         <div>
                                             {day > last ? day - last : day <= 0 ? prevlast + day : day}
                                         </div>
                                         <div className="schedule-area">
-                                            {rows.map((schedule,k) => (
+                                            {rows.map((schedule, k) => (
                                                 schedule.sch_date == year + '-' + zeroPadding(month) + '-' + zeroPadding(day) &&
-                                                    <div className='schedule-title' key={k} onClick={editHandleClickOpen} id={schedule.sch_id}>{schedule.sch_contents}</div>
-                                                
+                                                <div className='schedule-title' key={k} onClick={editHandleClickOpen} id={schedule.sch_id}>{schedule.sch_contents}</div>
+
                                             ))}
                                         </div>
                                     </div>
@@ -223,7 +241,7 @@ function Example(){
                     <DialogContentText>
                         スケジュール登録
                     </DialogContentText>
-                    <TextField margin="dense" id="sch_date" name="sch_date" label="予定日" type="text" fullWidth variant="standard" onChange={inputChange}/>
+                    <TextField margin="dense" id="sch_date" name="sch_date" label="予定日" type="text" fullWidth variant="standard" onChange={inputChange} />
                     <InputLabel id="sch_time_label">時刻</InputLabel>
                     <Select labelId="sch_hour" id="sch_hour_select" name="sch_hour" label="Hour" variant="standard" defaultValue="00" onChange={inputChange}>
                         <MenuItem value="00">00</MenuItem><MenuItem value="01">01</MenuItem>
@@ -237,7 +255,7 @@ function Example(){
                         <MenuItem value="案件">案件</MenuItem>
                         <MenuItem value="テスト">テスト</MenuItem>
                     </Select>
-                    <TextField margin="dense" id="sch_contents" name="sch_contents" label="内容" type="text" fullWidth variant="standard" onChange={inputChange}/>
+                    <TextField margin="dense" id="sch_contents" name="sch_contents" label="内容" type="text" fullWidth variant="standard" onChange={inputChange} />
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose}>Cancel</Button>
@@ -245,48 +263,49 @@ function Example(){
                 </DialogActions>
             </Dialog>
             <Dialog onClose={handleClose} open={editopen}>
-            <DialogTitle>Subscribe</DialogTitle>
-            <DialogContent>
-                <DialogContentText>
-                スケジュール更新
-                </DialogContentText>
-                <TextField margin="dense" id="sch_date" name="sch_date" label="予定日" type="text" fullWidth variant="standard" value={editData.sch_date} onChange={editChange}/>
-                <InputLabel id="sch_time_label">時刻</InputLabel>
-                <Select labelId="sch_hour" id="sch_hour_select" name="sch_hour" label="Hour" variant="standard"  value={editData.sch_hour} onChange={editChange}>
-                    <MenuItem value="00">00</MenuItem><MenuItem value="01">01</MenuItem>
-                </Select>
-                <Select labelId="sch_min" id="sch_min_select" name="sch_min" label="Min" variant="standard"  value={editData.sch_min} onChange={editChange}>
-                    <MenuItem value="00">00</MenuItem><MenuItem value="01">01</MenuItem>
-                </Select>
-                <InputLabel id="sch_category">カテゴリー</InputLabel>
-                <Select labelId="sch_category" id="sch_category_select" name="sch_category" label="Category" variant="standard"  value={editData.sch_category} onChange={editChange}>
-                    <MenuItem value="勉強">勉強</MenuItem>
-                    <MenuItem value="案件">案件</MenuItem>
-                    <MenuItem value="テスト">テスト</MenuItem>
-                </Select>
-                <TextField margin="dense" id="sch_contents" name="sch_contents" label="内容" type="text" fullWidth variant="standard"  value={editData.sch_contents} onChange={editChange}/>
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={editHandleClose}>Cancel</Button>
-                <Button href="/dashboard" onClick={updateSchedule}>Subscribe</Button>
-            </DialogActions>
-        </Dialog>
+                <DialogTitle>Subscribe</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        スケジュール更新
+                    </DialogContentText>
+                    <TextField margin="dense" id="sch_date" name="sch_date" label="予定日" type="text" fullWidth variant="standard" value={editData.sch_date} onChange={editChange} />
+                    <InputLabel id="sch_time_label">時刻</InputLabel>
+                    <Select labelId="sch_hour" id="sch_hour_select" name="sch_hour" label="Hour" variant="standard" value={editData.sch_hour} onChange={editChange}>
+                        <MenuItem value="00">00</MenuItem><MenuItem value="01">01</MenuItem>
+                    </Select>
+                    <Select labelId="sch_min" id="sch_min_select" name="sch_min" label="Min" variant="standard" value={editData.sch_min} onChange={editChange}>
+                        <MenuItem value="00">00</MenuItem><MenuItem value="01">01</MenuItem>
+                    </Select>
+                    <InputLabel id="sch_category">カテゴリー</InputLabel>
+                    <Select labelId="sch_category" id="sch_category_select" name="sch_category" label="Category" variant="standard" value={editData.sch_category} onChange={editChange}>
+                        <MenuItem value="勉強">勉強</MenuItem>
+                        <MenuItem value="案件">案件</MenuItem>
+                        <MenuItem value="テスト">テスト</MenuItem>
+                    </Select>
+                    <TextField margin="dense" id="sch_contents" name="sch_contents" label="内容" type="text" fullWidth variant="standard" value={editData.sch_contents} onChange={editChange} />
+                </DialogContent>
+                <DialogActions>
+                    <Button href="/dashboard" onClick={deletePost}>Delete</Button>
+                    <Button onClick={editHandleClose}>Cancel</Button>
+                    <Button href="/dashboard" onClick={updateSchedule}>Subscribe</Button>
+                </DialogActions>
+            </Dialog>
         </Fragment>
     );
 }
 
-function createCalendear(year,month){
-    const first = new Date(year,month - 1,1).getDay()
+function createCalendear(year, month) {
+    const first = new Date(year, month - 1, 1).getDay()
 
-    return [0,1,2,3,4,5].map((weekIndex) => {
-        return [0,1,2,3,4,5,6].map((dayIndex) => {
+    return [0, 1, 2, 3, 4, 5].map((weekIndex) => {
+        return [0, 1, 2, 3, 4, 5, 6].map((dayIndex) => {
             const day = dayIndex + 1 + weekIndex * 7
-            return day - first 
+            return day - first
         })
     })
 }
 
-function zeroPadding(num){
+function zeroPadding(num) {
     return ('0' + num).slice(-2);
 }
 
